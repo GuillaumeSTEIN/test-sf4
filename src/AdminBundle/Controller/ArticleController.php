b9d6bf7ef10d7552ce3f357bc2a531be91d7283b
Guillaume STEIN <guillaume.stein@gmail.com>
2018-02-24 14:32:58 +0100

    Create Article controller

---
+++ src/AdminBundle/Controller/ArticleController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\AdminBundle\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ArticleController extends Controller
+{
+}
