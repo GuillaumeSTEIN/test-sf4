54b751bfcb62f5f84c73ef5c10c0ba0ff1baa44b
Guillaume STEIN <guillaume.stein@gmail.com>
2018-02-24 15:32:14 +0100

    Basic creation page

--- src/AdminBundle/Controller/ArticleController.php
+++ src/AdminBundle/Controller/ArticleController.php
@@ -2,9 +2,33 @@
 
 namespace App\AdminBundle\Controller;
 
+use App\AdminBundle\Entity\Article;
+use App\AdminBundle\Form\ArticleType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ArticleController extends Controller
 {
+    /**
+     * @Route("article/create")
+     */
+    public function create(Request $request)
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->add('create',SubmitType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+        }
+
+        return $this->render('@Admin/article/create.html.twig',[
+            'form' => $form->createView(),
+        ]);
+    }
 }
